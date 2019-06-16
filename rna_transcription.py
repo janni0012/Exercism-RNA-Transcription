48551d57a280a93a9bd2fc315ac63a3e4ede2590
janni0012 <51456403+janni0012@users.noreply.github.com>
2019-06-16 19:26:03 +0200

    First Try

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,2 +1,4 @@
 def to_rna(dna_strand):
-    pass
+    transcription = { "G" : "C", "C" : "G", "T" : "A", "A" : "U"}
+    for  i in range(len(dna_strand)):
+        dna_strand[i] = transcription[dna_strand[i]]

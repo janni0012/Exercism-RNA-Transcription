f45ece9edc869a6ab16f170e02dac077a79e9f76
janni0012 <51456403+janni0012@users.noreply.github.com>
2019-06-16 19:56:39 +0200

    Final Change

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,6 +1,10 @@
 def to_rna(dna_strand):
     transcription = { "G" : "C", "C" : "G", "T" : "A", "A" : "U"}
     if len(dna_strand)>0:
+        rna = []
         for  i in range(len(dna_strand)):
-            dna_strand[i] = transcription[dna_strand[i]]
-            return dna_strand
+            rna.append(transcription[dna_strand[i]])
+        return "".join(rna)
+    else:
+        return ""
+    
